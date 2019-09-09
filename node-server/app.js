089a5da83b4c17adca817d468455e835bef387af
Daniel Peach <dpeaches96@gmail.com>
2019-09-09 16:07:52 -0400

    removed .idea and added mongodb

--- node-server/app.js
+++ node-server/app.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const cors = require('cors')
+const graphqlHTTP = require('express-graphql')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const infoLogger = require('easy-log')('app:info')
+const dbLogger = require('easy-log')('app:db')
 
-const app = express();
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds235401.mlab.com:35401/node-bucketlist`)
+mongoose.connection.once('open', () => {
+  dbLogger(`Connected to mongodb`)
+})
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+const app = express()
 
-module.exports = app;
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// app.use('/graphql', graphqlHTTP({
+//   schema,
+//   graphiql: true,
+// }))
+
+module.exports = app
