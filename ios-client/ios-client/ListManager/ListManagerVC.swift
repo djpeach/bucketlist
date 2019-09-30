daa114c015b4b1105444f386beb7adf7c6d2b09e
Daniel Peach <dpeaches96@gmail.com>
2019-09-30 14:18:23 -0400

    added skeleton app

---
+++ ios-client/ios-client/ListManager/ListManagerVC.swift
@@ -0,0 +1,17 @@
+//
+//  ListVC.swift
+//  ios-client
+//
+//  Created by Daniel Peach on 9/30/19.
+//  Copyright © 2019 Daniel Peach. All rights reserved.
+//
+
+import UIKit
+
+class ListManagerVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
