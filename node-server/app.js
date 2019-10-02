ff7b00165350273e8a8e10295bf5e959235b8345
Daniel Peach <dpeaches96@gmail.com>
2019-10-02 12:36:44 -0400

    added log for mongodb creds

--- node-server/app.js
+++ node-server/app.js
@@ -7,6 +7,8 @@ const infoLogger = require('easy-log')('app:info')
 const dbLogger = require('easy-log')('app:db')
 const schema = require('./graphql')
 
+dbLogger(`${ process.env.DB_USER }:${ process.env.DB_PW }`)
+
 mongoose.connect(`mongodb://${ process.env.DB_USER }:${ process.env.DB_PW }@ds235401.mlab.com:35401/node-bucketlist`)
 mongoose.connection.once('open', () => {
   dbLogger(`Connected to mongodb`)
