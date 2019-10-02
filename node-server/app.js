308d48ef4882d5b71173f4cc55e449b8b746d25f
Daniel Peach <dpeaches96@gmail.com>
2019-10-02 12:50:24 -0400

    worked, removing log

--- node-server/app.js
+++ node-server/app.js
@@ -7,8 +7,6 @@ const infoLogger = require('easy-log')('app:info')
 const dbLogger = require('easy-log')('app:db')
 const schema = require('./graphql')
 
-console.log(`${ process.env.DB_USER }:${ process.env.DB_PW }`)
-
 mongoose.connect(`mongodb://${ process.env.DB_USER }:${ process.env.DB_PW }@ds235401.mlab.com:35401/node-bucketlist`)
 mongoose.connection.once('open', () => {
   dbLogger(`Connected to mongodb`)
