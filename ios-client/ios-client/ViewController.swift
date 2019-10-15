b512488ff8dd5b5a5237aa92a152b303f2fa27ec
Daniel Peach <dpeaches96@gmail.com>
2019-10-15 11:19:21 -0400

    init ios proj with no storyboard

---
+++ ios-client/ios-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-client
+//
+//  Created by Peach, Daniel on 10/15/19.
+//  Copyright © 2019 Peach, Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
