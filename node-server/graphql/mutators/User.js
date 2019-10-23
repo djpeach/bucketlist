05edd71f59f033e4669d142b356ca86f670f6b32
Daniel Peach <danielpeach@149-162-181-135.dhcp-in.iupui.edu>
2019-10-23 16:56:32 -0400

    adding friends now adds to both users

--- node-server/graphql/mutators/User.js
+++ node-server/graphql/mutators/User.js
@@ -26,3 +26,22 @@ module.exports.deleteUser = {
     return UserModel.findByIdAndDelete(id)
   }
 }
+
+module.exports.addFriend = {
+  type: UserType,
+  args: {
+    friendId: { type: GraphQLID },
+    userId: { type: GraphQLID }
+  },
+  async resolve(parent, { friendId, userId }) {
+    gqlLogger(`adding friend with id ${friendId} to user with id: ${userId}`)
+    let user = UserModel.findById(userId)
+    await UserModel.findByIdAndUpdate(friendId, { "$push": {
+      "friends": userId
+    }})
+    await UserModel.findByIdAndUpdate(userId, { "$push": {
+      "friends": friendId
+    }})
+    return UserModel.findById(userId)
+  }
+}
