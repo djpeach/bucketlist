9da45468a00c9f5bc583d355fccdbed8a4e626d1
Daniel Peach <dpeaches96@gmail.com>
2019-10-23 22:19:56 -0400

    can delete friends

--- node-server/graphql/mutators/User.js
+++ node-server/graphql/mutators/User.js
@@ -30,16 +30,33 @@ module.exports.deleteUser = {
 module.exports.addFriend = {
   type: UserType,
   args: {
-    friendId: { type: GraphQLID },
-    userId: { type: GraphQLID }
+    userId: { type: GraphQLID },
+    friendId: { type: GraphQLID }
   },
   async resolve(parent, { friendId, userId }) {
     gqlLogger(`adding friend with id ${friendId} to user with id: ${userId}`)
-    let user = UserModel.findById(userId)
     await UserModel.findByIdAndUpdate(friendId, { "$push": {
       "friends": userId
     }})
     await UserModel.findByIdAndUpdate(userId, { "$push": {
+      "friends": friendId
+    }})
+    return UserModel.findById(userId)
+  }
+}
+
+module.exports.removeFriend = {
+  type: UserType,
+  args: {
+    userId: { type: GraphQLID },
+    friendId: { type: GraphQLID }
+  },
+  async resolve(parent, { friendId, userId }) {
+    gqlLogger(`removing friend with id ${friendId} to user with id: ${userId}`)
+    await UserModel.findByIdAndUpdate(friendId, { "$pull": {
+      "friends": userId
+    }})
+    let user = await UserModel.findByIdAndUpdate(userId, { "$pull": {
       "friends": friendId
     }})
     return UserModel.findById(userId)
