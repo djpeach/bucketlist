be577304f280c3ff57e7d7b54fc6e61532e5fd0d
Jayden Thrasher <JaydenThrasher@gmail.com>
2019-11-11 18:36:13 -0500

    Add user graphQL queries for ionic

--- ionic-client/src/graphql/accessors/User.js
+++ ionic-client/src/graphql/accessors/User.js
@@ -1,4 +1,4 @@
-import {gql} from 'apollo-boost'
+import { gql } from 'apollo-boost'
 
 export default {
   getAllUsers: gql`
@@ -10,5 +10,35 @@ export default {
       email
     }
   }
-`
+`,
+  getUserById: gql`
+  {
+    getUserById(id:ID!) {
+      id
+      firstName
+      lastName
+      email
+    }
+  }
+  `,
+  getUserByQuery: gql`
+  {
+    getUserByQuery(query:String!,limit: Int = 10) {
+      id
+      firstName
+      lastName
+      email
+    }
+  }
+  `,
+  getFriendsByQuery: gql`
+  {
+    getFriendsByQuery(userId: ID!, query: String!, limit: Int = 10) {
+      id
+      firstName
+      lastName
+      email
+    }
+  }
+  `
 }
