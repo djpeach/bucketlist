2fbe8ce6250126d475a25ebdb65e8669db4f9024
Jayden Thrasher <JaydenThrasher@gmail.com>
2019-11-16 15:31:01 -0500

    Fix typo

--- ionic-client/src/graphql/index.js
+++ ionic-client/src/graphql/index.js
@@ -3,7 +3,7 @@ import UserMutators from './mutators/User'
 import ListAccessors from './accessors/List'
 import ListMutators from './mutators/List'
 import ItemAccessors from './accessors/Item'
-import ItemMutators from './mutators/Items'
+import ItemMutators from './mutators/Item'
 
 export default {
   ...UserAccessors,
