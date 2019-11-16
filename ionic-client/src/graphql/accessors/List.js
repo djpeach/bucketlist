a8631edc837f6c7d66dead8332a1c165fe00356e
Jayden Thrasher <JaydenThrasher@gmail.com>
2019-11-16 14:24:50 -0500

    Add List accessors

---
+++ ionic-client/src/graphql/accessors/List.js
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-boost'
+
+export default {
+  getListsByUser: gql`
+  query ($id: ID!) {
+    getListsByUser(id: $id) {
+      id
+      title
+      items
+    }
+  }
+`,
+  getListsByQuery: gql`
+  query ($userId: ID!, $query: String!, $limit: Int = 10) {
+    getListsByQuery(id:$userId, query: $query, limit: $limit){
+      id
+      title
+      items
+    }
+  }
+  `
+}
