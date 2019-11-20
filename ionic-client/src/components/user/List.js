447a2e6c1c70850cea862f02bf48a27b76e106e2
Trenton Spice <tspice23@att.net>
2019-11-19 19:59:31 -0500

    list view wip

--- ionic-client/src/components/user/List.js
+++ ionic-client/src/components/user/List.js
@@ -1,19 +1,78 @@
 import React from 'react';
-import {IonPage} from "@ionic/react";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonGrid,
+  IonCol,
+} from "@ionic/react";
 
-import {lists} from '../../state';
+import firebase from "firebase";
+
+import {lists} from "../../state";
+
+import routes from "../../conf/routes";
+import {Redirect} from "react-router";
+
+const cardTitleStyle = {
+  padding: '10px',
+};
+
+const cardContentPadding = {
+  padding: '10px',
+};
 
 class List extends React.Component {
   render() {
     console.log(this.props.match.params)
     let list = lists[this.props.match.params.id];
 
-    return (
-      <IonPage>
-        <p>{list.name}</p>
-      </IonPage>
-    )
-  }
-}
+    if (!firebase.auth().currentUser) {
+      return (
+        <Redirect exact from={routes.index} to={routes.suggestions.create}/>
+      )
+    } else {
+      return (
+        <IonPage>
+          <IonHeader>
+            <IonToolbar color="primary">
+              <IonTitle>BucketList</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonGrid>
+              <IonCol size="12" size-sm="6">
+                <IonCard>
+                  <IonTitle style={cardTitleStyle}>{list.name}</IonTitle>
+                  <IonList>
+                      { (list.items.length > 0) ?
+                        list.items.map((item) => {
+                          return (
+                            <IonItem key={item.message}>
+                              <IonLabel>
+                              {item.from ? (<p>From: {item.from}</p>) : null} 
+                              {item.link ? (<a href={item.link} target="_"><h3>{item.message}</h3></a>) : <h3>{item.message}</h3>}
+                              </IonLabel>
+                            </IonItem>
+                          );
+                        }) :
+                        <p style={cardContentPadding}> No items </p>
+                      }
+                    </IonList>
+                </IonCard>
+              </IonCol>
+            </IonGrid>
+          </IonContent>
+        </IonPage>
+      );
+    };
+  };
+};
 
-export default List
+export default List;
