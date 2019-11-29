0c9abd028b6ec94b8f6a46a171c6441f4b25ba8d
Trenton Spice <tspice23@att.net>
2019-11-29 14:14:48 -0500

    updated list with nav and back button

--- ionic-client/src/components/user/List.js
+++ ionic-client/src/components/user/List.js
@@ -11,67 +11,56 @@ import {
   IonLabel,
   IonGrid,
   IonCol,
+  IonButton,
 } from "@ionic/react";
-
-import firebase from "firebase";
-
 import {lists} from "../../state";
-
+import MdArrowDropleft from 'react-ionicons/lib/MdArrowDropleft'
 import routes from "../../conf/routes";
-import {Redirect} from "react-router";
-
-const cardTitleStyle = {
-  padding: '10px',
-};
-
-const cardContentPadding = {
-  padding: '10px',
-};
 
 class List extends React.Component {
   render() {
-    console.log(this.props.match.params)
     let list = lists[this.props.match.params.id];
-
-    if (!firebase.auth().currentUser) {
-      return (
-        <Redirect exact from={routes.index} to={routes.suggestions.create}/>
-      )
-    } else {
-      return (
-        <IonPage>
-          <IonHeader>
-            <IonToolbar color="primary">
-              <IonTitle>BucketList</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent>
-            <IonGrid>
-              <IonCol size="12" size-sm="6">
-                <IonCard>
-                  <IonTitle style={cardTitleStyle}>{list.name}</IonTitle>
-                  <IonList>
-                      { (list.items.length > 0) ?
-                        list.items.map((item) => {
-                          return (
-                            <IonItem key={item.message}>
-                              <IonLabel>
-                              {item.from ? (<p>From: {item.from}</p>) : null} 
-                              {item.link ? (<a href={item.link} target="_"><h3>{item.message}</h3></a>) : <h3>{item.message}</h3>}
-                              </IonLabel>
-                            </IonItem>
-                          );
-                        }) :
-                        <p style={cardContentPadding}> No items </p>
-                      }
-                    </IonList>
-                </IonCard>
-              </IonCol>
-            </IonGrid>
-          </IonContent>
-        </IonPage>
-      );
-    };
+    return (
+      <IonPage>
+        <IonHeader>
+        <IonToolbar color="primary">
+        <div className="bl-icon-div">
+          <BucketListIcon className="bl-svg-icon"/>
+        </div>
+        <IonTitle className="bl-nav-title">BucketList</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+        <IonContent>
+          <IonGrid>
+            <IonCol size="12" size-sm="6">
+            <IonButton color="light" type="button"
+                       routerLink={routes.home}
+                       className="bl-list-back-btn">
+              <MdArrowDropleft /> Back
+            </IonButton>
+              <IonCard>
+                <IonTitle className="bl-list-title">{list.name}</IonTitle>
+                <IonList>
+                    { (list.items.length > 0) ?
+                      list.items.map((item) => {
+                        return (
+                          <IonItem key={item.message}>
+                            <IonLabel>
+                            {item.from ? (<p>From: {item.from}</p>) : null} 
+                            {item.link ? (<a href={item.link} target="_"><h3>{item.message}</h3></a>) : <h3>{item.message}</h3>}
+                            </IonLabel>
+                          </IonItem>
+                        );
+                      }) :
+                      <p className="bl-card-padding"> No items </p>
+                    }
+                  </IonList>
+              </IonCard>
+            </IonCol>
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+    );
   };
 };
 
