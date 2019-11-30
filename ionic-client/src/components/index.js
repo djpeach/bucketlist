512da821897e749b8587fb81ff63eda74fd973df
Trenton Spice <tspice23@att.net>
2019-11-30 18:12:01 -0500

    NewSuggestion -> NewDrop

--- ionic-client/src/components/index.js
+++ ionic-client/src/components/index.js
@@ -1,8 +1,8 @@
 import Dashboard from './user/Dashboard'
 import List from './user/List'
-import NewSuggestion from './user/NewSuggestion'
+import NewDrop from './user/NewDrop'
 import More from './user/More'
 import Login from './auth/Login'
 import Register from './auth/Register'
 
-export { Dashboard, List, NewSuggestion, More, Login, Register }
+export { Dashboard, List, NewDrop, More, Login, Register }
