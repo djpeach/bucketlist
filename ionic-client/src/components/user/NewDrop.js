cc79d6b67ec23bcb00e36d1d25f84820219cdfa2
Daniel Peach <dpeaches96@gmail.com>
2019-12-01 17:19:13 -0500

    using real friends

--- ionic-client/src/components/user/NewDrop.js
+++ ionic-client/src/components/user/NewDrop.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import {
   IonPage,
   IonCard,
@@ -10,12 +9,10 @@ import {
 } from '@ionic/react'
 import Select from 'react-select';
 import authedComponent from '../common/AuthedComponent'
-import {state as userState} from "../../state";
-
-userState.user.friends.map((obj) => {
-  obj.value = obj.email
-  obj.label = `${obj.firstName} ${obj.lastName}`
-})
+import React, {useState} from 'react'
+import {useQuery} from '@apollo/react-hooks'
+import gql from '../../graphql'
+import firebase from 'firebase'
 
 const friendSearchStyles = {
   menu: (provided, state) => ({
@@ -28,64 +25,58 @@ const friendSearchStyles = {
   }),
 }
 
-class NewDrop extends React.Component {
+function NewDrop(props) {
 
-  constructor(props) {
-    super(props);
+  const [friendObj, setFriendObj] = useState(null)
+  const [message, setMessage] = useState('')
+  const {loading, error, data} = useQuery(gql.getAllFriends, {variables: {userId: firebase.auth().currentUser.uid}})
 
-    this.state = {
-      friendObj: null,
-      message: ''
-    }
+  const handleFriendSearchChange = friendObj => {
+    setFriendObj(friendObj)
   }
 
-  handleFriendSearchChange = friendObj => {
-    this.setState({
-      friendObj,
-    })
-  }
-
-  handleDescriptionChange = (event) => {
+  const handleDescriptionChange = (event) => {
     var description = event.target.value
     // Handle newlines in the description textarea (thinking either <br /> or \n)
     description = description.replace(/\r?\n/g, ' <br /> ')
-    this.setState({
-      message: description
-    })
+    setMessage(description)
   }
 
-  render() {
-    const { friendObj } = this.state;
-    return (
-      <IonPage className="bl-page">
-        <IonCard className="bl-card-padding">
-          <h1 style={{ paddingBottom: '20px' }}>New Drop</h1>
+  const friends = (loading || error || data.getAllFriends.length <= 0 ? [{id: 0, firstName: 'No', lastName: ' Friends :('}] : data.getAllFriends)
+  friends.map((obj) => {
+    obj.value = obj.id
+    obj.label = `${obj.firstName} ${obj.lastName}`
+  })
 
-          <Select
-            value={friendObj}
-            placeholder='Search Friends'
-            styles={friendSearchStyles}
-            onChange={this.handleFriendSearchChange}
-            noOptionsMessage={() => 'Friend not found'}
-            isClearable
-            isSearchable
-            name="friends"
-            // TODO: set this up to not use mock state (options are label, value)
-            options={userState.user.friends}
-          />
+  return (
+    <IonPage className="bl-page">
+      <IonCard className="bl-card-padding">
+        <h1 style={{ paddingBottom: '20px' }}>New Drop</h1>
+
+        <Select
+          value={friendObj}
+          placeholder='Search Friends'
+          styles={friendSearchStyles}
+          onChange={handleFriendSearchChange}
+          noOptionsMessage={() => 'Friend not found'}
+          isClearable
+          isSearchable
+          name="friends"
+          // TODO: set this up to not use mock state (options are label, value)
+          options={friends}
+        />
 
 
-          <IonItem style={{ marginTop: '20px' }}>
-            <IonLabel position="floating"></IonLabel>
-            <IonTextarea rows={1} cols={20} autoGrow={true} placeholder={"Description"} onIonChange={this.handleDescriptionChange}></IonTextarea>
-          </IonItem>
-          <IonButton style={{ marginTop: '20px' }} onClick={() => {console.log(this.state)}}>
-            Send Drop
-          </IonButton>
-        </IonCard>
-      </IonPage>
-    )
-  }
+        <IonItem style={{ marginTop: '20px' }}>
+          <IonLabel position="floating"></IonLabel>
+          <IonTextarea rows={1} cols={20} autoGrow={true} placeholder={"Description"} onIonChange={handleDescriptionChange}></IonTextarea>
+        </IonItem>
+        <IonButton style={{ marginTop: '20px' }} onClick={() => {console.log(friendObj, message)}}>
+          Send Drop
+        </IonButton>
+      </IonCard>
+    </IonPage>
+  )
 }
 
 export default authedComponent(NewDrop)
