65ee17da7df3f3133b4042aab25a04e784aa4def
Daniel Peach <dpeaches96@gmail.com>
2019-12-01 12:35:50 -0500

    ready to add real user info

--- ionic-client/src/components/user/ProfileSettings.js
+++ ionic-client/src/components/user/ProfileSettings.js
@@ -10,68 +10,30 @@ import {
   IonContent,
 } from '@ionic/react'
 import firebase from 'firebase'
-import { state } from '../../state'
 import routes from '../../conf/routes'
-import { flowRight as compose } from 'lodash'
-import { graphql } from 'react-apollo'
-import graphqlQueries from '../../graphql'
+import gql from '../../graphql'
 import authedComponent from '../common/AuthedComponent'
 
-const ListUsers = ({ getAllusers }) => {
-  if (getAllusers.loading) {
-    return <IonTitle>Loading . . .</IonTitle>
-  } else if (getAllusers.error) {
-    return <IonTitle>{getAllusers.error.message}</IonTitle>
-  } else {
-    const { getAllUsers: users } = getAllusers
-    return (
-      <IonItem>
-        <IonList>
-          {users.map((user) => {
-            return (
-              <IonItem key={user.id}>
-                <IonLabel>Id: {user.id}</IonLabel>
-                <IonLabel>
-                  Name: {user.firstName} {user.lastName}
-                </IonLabel>
-                <IonLabel>Email: {user.email}</IonLabel>
-              </IonItem>
-            )
-          })}
-        </IonList>
-      </IonItem>
-    )
+function ProfileSettings(props) {
+  const logout = () => {
+    firebase.auth().signOut().then(() => {
+      props.history.push(routes.auth.login)
+    })
   }
+
+  return (
+    <IonPage className="bl-page">
+      <IonContent>
+        <IonCard className={"bl-card-padding"}>
+          <h1> User Information </h1>
+          {/*<p>Name: {state.user.firstName} {state.user.lastName}</p>*/}
+          {/*<p>Email: {state.user.email}</p>*/}
+          {/*<p>UserID: {state.user.uid}</p>*/}
+        </IonCard>
+      </IonContent>
+      <IonButton onClick={logout} style={{ marginBottom: '20px' }}>Logout</IonButton>
+    </IonPage>
+  )
 }
 
-class ProfileSettings extends React.Component {
-  logout = () => {
-    firebase.auth().signOut()
-    state.user = null
-    return this.props.history.push(routes.auth.login)
-  }
-
-  render() {
-    const { getAllUsers } = this.props
-    console.log(getAllUsers)
-    return (
-      <IonPage className="bl-page">
-        <IonContent>
-          <IonCard className={"bl-card-padding"}>
-            <h1> User Information </h1>
-            <p>Name: {state.user.firstName} {state.user.lastName}</p>
-            <p>Email: {state.user.email}</p>
-            <p>UserID: {state.user.uid}</p>
-          </IonCard>
-          <ListUsers getAllusers={getAllUsers} />
-        </IonContent>
-        <IonButton onClick={this.logout} style={{ marginBottom: '20px' }}>Logout</IonButton>
-      </IonPage>
-    )
-  }
-}
-
-export default compose(
-  graphql(graphqlQueries.getAllUsers, { name: 'getAllUsers' }),
-  authedComponent
-)(ProfileSettings)
+export default authedComponent(ProfileSettings)
