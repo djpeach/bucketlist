e6ecbe64862574df1eacba249b28908e55c5b91f
Trenton Spice <tspice23@att.net>
2019-12-01 18:55:39 -0500

    list view working with gql

--- ionic-client/src/components/user/List.js
+++ ionic-client/src/components/user/List.js
@@ -15,52 +15,92 @@ import {
 import { lists } from '../../state'
 import MdArrowDropleft from 'react-ionicons/lib/MdArrowDropleft'
 import routes from '../../conf/routes'
+import { useQuery } from '@apollo/react-hooks';
+import gql from '../../graphql'
+import firebase from 'firebase'
 
-class List extends React.Component {
-  render() {
-    let list = lists[this.props.match.params.id]
+function ListView(props) {
+  const {loading, error, data} = useQuery(gql.getListsByQuery, {variables: {userId: firebase.auth().currentUser.uid, query: props.title}})
+
+  if (loading) {
     return (
-      <IonPage className="bl-page">
-        <IonContent>
-          <IonGrid>
-            <IonCol size="12" size-sm="6">
-              <IonButton
-                color="light"
-                type="button"
-                routerLink={routes.home}
-                className="bl-list-back-btn">
-                <MdArrowDropleft /> Back
-              </IonButton>
-              <IonCard>
-                <IonTitle className="bl-list-title">{list.name}</IonTitle>
-                <IonList>
-                  {list.items.length > 0 ? (
-                    list.items.map((item) => {
-                      return (
-                        <IonItem key={item.message}>
-                          <IonLabel>
-                            {item.from ? <p>From: {item.from}</p> : null}
-                            {item.link ? (
-                              <a href={item.link} target="_">
-                                <h3>{item.message}</h3>
-                              </a>
-                            ) : (
-                              <h3>{item.message}</h3>
-                            )}
-                          </IonLabel>
-                        </IonItem>
-                      )
-                    })
-                  ) : (
-                    <p className="bl-card-padding"> No items </p>
-                  )}
-                </IonList>
-              </IonCard>
-            </IonCol>
-          </IonGrid>
-        </IonContent>
-      </IonPage>
+      <IonList>
+        <IonItem>
+          <IonLabel>
+            Loading Lists ...
+          </IonLabel>
+        </IonItem>
+      </IonList>
     )
   }
-}
+
+  console.log(error)
+
+  if (error) {
+    return (
+      <IonList>
+        <IonItem>
+          <IonLabel>
+           Error: {error.message}
+          </IonLabel>
+        </IonItem>
+      </IonList>
+    )
+  }
+
+  console.log(data)
+
+  return (
+    <IonList>
+      {console.log((Array.isArray(data.getListsByQuery[0].items) && data.getListsByQuery[0].items.length))}
+      {(Array.isArray(data.getListsByQuery[0].items) && data.getListsByQuery[0].items.length) ? (
+        data.getListsByQuery[0].items.map((item) => {
+          return (
+            <IonItem key={item.message}>
+              <IonLabel>
+                {item.from ? <p>From: {item.from.firstName} {item.from.lastName}</p> : null}
+                {item.link ? (
+                  <a href={item.link} target="_">
+                    <h3>{item.message}</h3>
+                  </a>
+                ) : (
+                  <h3>{item.message}</h3>
+                )}
+              </IonLabel>
+            </IonItem>
+          )
+        })
+      ) : (
+        <p className="bl-card-padding"> No items </p>
+      )}
+    </IonList>
+  )
+};
+
+
+function List() {
+  const title = window.location.href.split('/lists/')[1]
+  return (
+    <IonPage className="bl-page">
+      <IonContent>
+        <IonGrid>
+          <IonCol size="12" size-sm="6">
+            <IonButton
+              color="light"
+              type="button"
+              routerLink={routes.home}
+              className="bl-list-back-btn">
+              <MdArrowDropleft /> Back
+            </IonButton>
+            <IonCard>
+              <IonTitle className="bl-list-title">{title}</IonTitle>
+              <ListView title={title} />
+            </IonCard>
+          </IonCol>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  )
+};
+
 export default authedComponent(List)
